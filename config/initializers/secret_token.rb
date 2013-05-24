77950da66a549377e9059c0d04f0f2d8485b06e3
Jason LaGreca <jlagreca629@gmail.com>
2013-05-24 08:50:46 -0400

    created opening status update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Familybook::Application.config.secret_token = '3d0a4b5c611daba3d4fcd2430e03b62595c740ba1226e2fc500663cdfdc820ad0a45a4b7127b5a5e34a410c2afc4ea753c58e093c4c3b2e567455f9f4749b080'
